03e55d458c98788375bee5d75375ca1e052c2852
brpuneet898 <90244927+brpuneet898@users.noreply.github.com>
2024-12-03 16:30:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,6 +156,8 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 from supabase import create_client, Client
 
+from streamlit_analytics import st_analytics
+
 GA_ID = "G-HSVC2DMLZW"
 st_analytics(GA_ID=GA_ID)
 st_analytics.track_event("Page Opened", {"event": "Page Loaded"})
