fb56c85971c511cae470baed6324dfe5597e8844
brpuneet898 <90244927+brpuneet898@users.noreply.github.com>
2024-12-03 15:58:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,12 +156,26 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 from supabase import create_client, Client
 
-google_analytics_file = "google_analytics.js"  
+# google_analytics_file = "google_analytics.js"  
 
-with open(google_analytics_file, 'r') as file:
-    google_analytics_script = file.read()
+# with open(google_analytics_file, 'r') as file:
+#     google_analytics_script = file.read()
 
-st.markdown(f'<script>{google_analytics_script}</script>', unsafe_allow_html=True)
+# st.markdown(f'<script>{google_analytics_script}</script>', unsafe_allow_html=True)
+
+google_analytics_script = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HSVC2DMLZW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HSVC2DMLZW');
+</script>
+"""
+
+st.markdown(google_analytics_script, unsafe_allow_html=True)
 
 url = "https://armzsxwnhybsgedffijs.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFybXpzeHduaHlic2dlZGZmaWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwODcxMzEsImV4cCI6MjA0ODY2MzEzMX0.g7Ty0qNFCVJiEp38IQ_Uw9yEn4jzA67XPsLCmQ8f26o"
